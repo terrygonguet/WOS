174bef0d682e76da4b7f7dc3fe9ea25c7424d65a
terrygonguet <terry@gonguet.com>
2017-11-20 21:50:35 +0000

    More cv hero stuff

--- static/home/apps/cv/cv.js
+++ static/home/apps/cv/cv.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   var blinky = setInterval(function () {
-    $("#blinky").toggle();
+    var newtxt = $("#blinky").html() === "&nbsp;&nbsp;" ? "&nbsp;_" : "&nbsp;&nbsp;";
+    $("#blinky").html(newtxt);
   }, 750);
 
   var birth = new Date(1996, 04, 11);
   $("#age").text(parseInt((Date.now() - birth) / 31557600000));
+
+  $("#hidetext").click(function () {
+    $("#name, #hero>h3").fadeToggle();
+    $(this).toggleClass("fa-eye fa-eye-slash");
+  });
 });
