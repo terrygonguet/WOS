4384aa06ad156de01edcc7af18323a3b4f52f4ad
Terry Gonguet <terry@gonguet.com>
2018-01-03 15:53:29 +0100

    Added API location

--- static/js/wos.js
+++ static/js/wos.js
@@ -1,6 +1,7 @@
 var WOS = WOS || {};
 
 WOS.io = io("http://ca2-imd-wos.herokuapp.com/");
+WOS.api = "http://ca2-imd-wos.herokuapp.com/";
 
 $(document).ready(function () {
   $(document).keydown(function (e) {
